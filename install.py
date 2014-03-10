eda24d65a170302330b2941d51de96e14646a3ba
Tim van der Meij <timvandermeij@gmail.com>
2014-03-10 19:42:13 +0100

    Upgraded install.py with listener node SDCC changes, updated both Makefiles to make a better 'clean' target

--- install.py
+++ install.py
@@ -3,26 +3,25 @@ import shutil
 import urllib2
 import zipfile
 
-# Sources and folders
+# Constants (sources and folders)
 MULTI_SPIN_URL = 'https://sites.google.com/site/boccamaurizio/multi-Spin_v2.0.zip'
 CC_USB_FIRMWARE_URL = 'http://www.ti.com/lit/sw/swrc088c/swrc088c.zip'
-
 MULTI_SPIN_FOLDER = 'multi-spin-2.0'
 CC_USB_FIRMWARE_FOLDER= 'cc-usb-firmware'
-
 LISTEN_NODE_FOLDER = 'listen-node-project'
 RF_NODE_FOLDER = 'rf-node-project'
 PATCHES_FOLDER = 'patches'
-
 LIBRARIES_FOLDER = 'libraries'
 PROJECTS_FOLDER = 'projects'
-
 DOWNLOADS = [MULTI_SPIN_URL, CC_USB_FIRMWARE_URL]
 LIBRARIES = [MULTI_SPIN_FOLDER, CC_USB_FIRMWARE_FOLDER]
 PROJECTS = [LISTEN_NODE_FOLDER, RF_NODE_FOLDER]
 
-# Get the installation path
+# Paths
 installation_path = raw_input('Enter the installation path: ')
+libraries_path = os.path.join(installation_path, LIBRARIES_FOLDER)
+multi_spin_path = os.path.join(libraries_path, MULTI_SPIN_FOLDER)
+cc_usb_firmware_path = os.path.join(libraries_path, CC_USB_FIRMWARE_FOLDER)
 
 # Check if the installation path exists or create it if possible
 if not os.path.exists(installation_path):
@@ -48,7 +47,7 @@ if os.path.exists(installation_path):
 # Download the Multi-Spin 2.0 code and the CC USB firmware
 for index, download in enumerate(DOWNLOADS):
     try:
-        # Do not use the Python user agent as TI blocks it
+        # Do not use the Python user agent because TI blocks it
         request = urllib2.Request(download, headers = {'User-Agent': ''})
         response = urllib2.urlopen(request)
         output = open(LIBRARIES[index] + '.zip', 'wb')
@@ -60,7 +59,6 @@ for index, download in enumerate(DOWNLOADS):
         os._exit(1)
 
 # Unzip the libraries and place the code in the installation path
-libraries_path = os.path.join(installation_path, LIBRARIES_FOLDER)
 for library in LIBRARIES:
     try:
         zip_file = zipfile.ZipFile(open(library + '.zip', 'rb'))
@@ -73,24 +71,33 @@ for library in LIBRARIES:
         os._exit(1)
 
 # Apply the patches for LED support and SDCC compilation.
-multi_spin_path = os.path.join(libraries_path, MULTI_SPIN_FOLDER)
-cc_usb_firmware_path = os.path.join(libraries_path, CC_USB_FIRMWARE_FOLDER)
-try:
-    shutil.copy(PATCHES_FOLDER + '/leds.c', multi_spin_path + '/multi-Spin_v2.0/xpand2531/leds.c')
-    shutil.copy(PATCHES_FOLDER + '/ioCC2530.h', libraries_path + '/ioCC2530.h')
-    shutil.copy(PATCHES_FOLDER + '/ioCC2531.h', libraries_path + '/ioCC2531.h')
-    shutil.copy(PATCHES_FOLDER + '/rf.c', multi_spin_path + '/multi-Spin_v2.0/xpand2531/rf.c')
-    shutil.copy(PATCHES_FOLDER + '/rf.h', multi_spin_path + '/multi-Spin_v2.0/xpand2531/rf.h')
-    shutil.copy(PATCHES_FOLDER + '/timers34.c', multi_spin_path + '/multi-Spin_v2.0/xpand2531/timers34.c')
-    shutil.copy(PATCHES_FOLDER + '/timers34.h', multi_spin_path + '/multi-Spin_v2.0/xpand2531/timers34.h')
-    shutil.copy(PATCHES_FOLDER + '/flash.c', multi_spin_path + '/multi-Spin_v2.0/xpand2531/flash.c')
-    shutil.copy(PATCHES_FOLDER + '/main_rf_node.c', multi_spin_path + '/multi-Spin_v2.0/RF_sensor/main.c')
-    shutil.copy(PATCHES_FOLDER + '/hal_types.h', cc_usb_firmware_path + '/source/components/common/hal_types.h')
-    shutil.copy(PATCHES_FOLDER + '/hal_int.h', cc_usb_firmware_path + '/source/components/targets/interface/hal_int.h')
-    print('-> Applied patches')
-except shutil.Error:
-    print('-> Could not apply all patches; aborting!')
-    os._exit(1)
+PATCHES = {'clock.c': cc_usb_firmware_path + '/source/components/radios/cc2530/clock.c',
+           'flash.c': multi_spin_path + '/multi-Spin_v2.0/xpand2531/flash.c',
+           'hal_board.h': cc_usb_firmware_path + '/source/components/targets/cc2531_dongle/hal_board.h',
+           'hal_cc8051.h': cc_usb_firmware_path + '/source/components/common/cc8051/hal_cc8051.h',
+           'hal_int.h': cc_usb_firmware_path + '/source/components/targets/interface/hal_int.h',
+           'hal_mcu.c': cc_usb_firmware_path + '/source/components/radios/cc2530/hal_mcu.c',
+           'hal_types.h': cc_usb_firmware_path + '/source/components/common/hal_types.h',
+           'ioCC2530.h': libraries_path + '/ioCC2530.h',
+           'ioCC2531.h': libraries_path + '/ioCC2531.h',
+           'leds.c': multi_spin_path + '/multi-Spin_v2.0/xpand2531/leds.c',
+           'main_listen_node.c': multi_spin_path + '/multi-Spin_v2.0/listen_node/main.c',
+           'main_rf_node.c': multi_spin_path + '/multi-Spin_v2.0/RF_sensor/main.c',
+           'rf.c': multi_spin_path + '/multi-Spin_v2.0/xpand2531/rf.c',
+           'rf.h': multi_spin_path + '/multi-Spin_v2.0/xpand2531/rf.h',
+           'timers34.c': multi_spin_path + '/multi-Spin_v2.0/xpand2531/timers34.c',
+           'timers34.h': multi_spin_path + '/multi-Spin_v2.0/xpand2531/timers34.h',
+           'usb_framework.h': cc_usb_firmware_path + '/source/components/usb/library/usb_framework.h',
+           'usb_interrupt.h': cc_usb_firmware_path + '/source/components/usb/library/usb_interrupt.h',
+           'usb_uart.c': cc_usb_firmware_path + '/source/components/usb/class_cdc/usb_uart.c'}
+
+for source, destination in PATCHES.iteritems():
+    try:
+        shutil.copy(PATCHES_FOLDER + '/' + source, destination)
+        print('-> Patched "' + source + '"')
+    except shutil.Error:
+        print('-> Could not apply all patches; aborting!')
+        os._exit(1)
 
 # Copy the projects to the installation path
 projects_path = os.path.join(installation_path, PROJECTS_FOLDER)
